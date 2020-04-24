69d58df888955bac618e1af36064eff7fac6c093
Jon Canning <jon.canning@gmail.com>
2020-04-24 11:57:11 +0100

    My attempt

--- DetectDuplicatesFSharp/Program.fs
+++ DetectDuplicatesFSharp/Program.fs
@@ -1,81 +1,49 @@
 ﻿open System.IO
-open System.Linq
 open System.Text.RegularExpressions
 
-type FileReader = string -> Result<string seq,exn>
+let output (packageName, duplicates) =
+    duplicates
+    |> Seq.head
+    |> fst
+    |> printfn "Duplicate %s found in %s" packageName
 
-let output (data:seq<string*string>) =
-    data 
-    |> Seq.iter (fun (key,value) -> printfn "Duplicate %s found in %s" key value)
+let getPath args =
+    args
+    |> Array.tryHead
+    |> Option.defaultValue (Directory.GetCurrentDirectory())
 
-let getPath (args: string []) =
-    match args.Any() with
-    | true -> args.[0]
-    | false -> Directory.GetCurrentDirectory()
+let getFileText fileName = fileName, File.ReadAllText fileName
 
 let getFiles path =
     printfn "Looking for duplicates in %s" path
     Directory.GetFiles(path, "*.csproj", SearchOption.AllDirectories)
 
-let readFile : FileReader =
-    fun path ->
-        try
-            seq { use reader = new StreamReader(File.OpenRead(path))
-                  while not reader.EndOfStream do
-                      yield reader.ReadLine() }
-            |> Ok
-        with
-        | ex -> Error ex
-        
-let parseLine (fileName:string) (line:string)  =
-    let regexMatch = Regex.Match(line, "PackageReference Include=\"([^\"]*)\"")
-    match regexMatch.Success with
-    | true -> 
-        Some (regexMatch.Groups.[1].Value, fileName)
-    | _ -> None
+let parseFile (fileName, text) =
+    Regex.Matches(text, "PackageReference Include=\"(.+)\"")
+    |> Seq.cast<Match>
+    |> Seq.choose (fun mtch -> mtch.Groups |> Seq.tryItem 1)
+    |> Seq.map (fun group -> fileName, group.Value)
 
-let parse (fileName:string) (data:string seq) =
-    data
-    |> Seq.map (fun x-> parseLine fileName x)
-    |> Seq.choose id
-    
-    
-let findDuplicates (packages:seq<string * string>) =
-    packages
-    |> Seq.groupBy (fun s -> s)
-    |> Seq.filter (fun (key, values) -> (values.Count() >= 2)) 
-    |> Seq.map (fun(key,_)-> key)
-    
-let checkFile (file: string) =
-    let fileName = Path.GetFileName(file)
-    try
-        match file |> readFile with
-        | Ok data -> data |> parse fileName |> findDuplicates   
-        | Error ex ->   printfn "Error: %A" ex.Message
-                        Seq.empty<string*string>
-    with
-    | ex -> printfn "Error processing entry in %s" fileName
-            Seq.empty<string*string>
+let hasDuplicates group =
+    group
+    |> snd
+    |> Seq.length > 1
 
-let loopFiles (files: string []) =
-    Array.map (fun x -> checkFile x) files
-
-let processProjectFiles (args: string []) =
+let processProjectFiles args =
     args
     |> getPath
     |> getFiles
-    |> loopFiles
-    |> Seq.collect (fun x->x)
+    |> Seq.map getFileText
+    |> Seq.collect parseFile
+    |> Seq.groupBy snd
+    |> Seq.filter hasDuplicates
 
 [<EntryPoint>]
-let main argv =
-
-    let dups = processProjectFiles argv
-    dups |> output
-
+let main args =
+    let duplicates = processProjectFiles args
     printfn "Duplicate detection complete"
-    
-    if dups.Any() then
+    match duplicates |> Seq.isEmpty with
+    | true -> 0
+    | _ ->
+        duplicates |> Seq.iter output
         1
-    else
-        0
